436e1be1f9f283cdcb4f2bbb603f66c6fd85915a
RangaReddy12 <pranga2010@gmail.com>
2026-04-01 18:20:37 +0530

    Remove tag

--- src/test/java/testRunner/AppTest.java
+++ src/test/java/testRunner/AppTest.java
@@ -9,7 +9,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @RunWith(Cucumber.class)
 @CucumberOptions(dryRun = false,
 features = {"./featureFiles/ERP.feature"},
-monochrome = true,glue = {"stepDefination"},tags = ("@Customers,@Suppliers"),
+monochrome = true,glue = {"stepDefination"},tags = ("@Customers not,@Suppliers"),
 plugin = {"pretty","html:target/report/cucumber.html"})
 public class AppTest extends AbstractTestNGCucumberTests{
 
